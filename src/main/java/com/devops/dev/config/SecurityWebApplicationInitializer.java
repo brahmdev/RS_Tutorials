fcd40cc27304474642ec1da21544c8f205777edc
brahmdev <pandey.dev25@gmail.com>
2017-04-15 14:05:30 +0200

    SecurityWebApplicationInitializer Added

---
+++ src/main/java/com/devops/dev/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.devops.dev.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends
+		AbstractSecurityWebApplicationInitializer {
+}
