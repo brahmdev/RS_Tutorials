a3cb09c368863d56091d0a82c6898fa763562706
brahmdev <pandey.dev25@gmail.com>
2017-04-15 19:22:27 +0200

    WebSecurity Updated to all request other than only /

--- src/main/java/com/devops/dev/config/WebSecurityConfig.java
+++ src/main/java/com/devops/dev/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	
     	 http
     	 .authorizeRequests()
-    	 .antMatchers("/").permitAll() 
+    	 .antMatchers("/**").permitAll() 
     	 /*.antMatchers("/admin/**").hasRole("ADMIN")*/
     	 .anyRequest().authenticated()
     	 .and()
